d27c3864c9e415b551810066133b00a438c032f9
Aaron Targa <33754619+AaronTarga@users.noreply.github.com>
2020-12-24 19:27:07 +0100

    add config file and change to geckodriver

--- README.md
+++ README.md
@@ -2,8 +2,21 @@
 This is a python script made to collect steam trading cards by clicking through list during steam sales.
 
 For this script you need to install selenium on python. [Here are instructions to download selenium for python.](https://selenium-python.readthedocs.io/installation.html)
-Download and use chrome driver as instructed in the documentation from selenium.
+Download and use chrome driver or geckodriver as instructed in the documentation from selenium.
 
 Then you need to change some things in the file:
-Set the value of the cookies in the file by copying a cookie while signed in in the browser. You can do that with the firefox or chrome web developer tools. But it's more convenient if you use a plugin like [Cookie Editor](https://addons.mozilla.org/de/firefox/addon/cookie-editor/).
-Ather that you can start your script by executing it like a  normal python script `python steamv2.py`.
+Set the value of the cookies in the config.yml file by copying cookies while signed in, in the browser. You can do that with the firefox or chrome web developer tools. But it's more convenient if you use a plugin like [Cookie Editor](https://addons.mozilla.org/de/firefox/addon/cookie-editor/).
+After that you can start your script by executing it like a normal python script `python steamv2.py`.
+
+Example config.yml:
+```yaml
+steam:
+  sessionId: some_sesssionId
+  steamLoginSecure: some_steamLoginSecure
+
+loops:
+  first: 1
+  second: 12
+```
+
+
--- steamv2.py
+++ steamv2.py
@@ -1,25 +1,35 @@
 from selenium import webdriver
 import time
+import yaml
 
-options = webdriver.ChromeOptions()
-options.add_argument("headless")
+with open("config.yml", "r") as ymlfile:
+    cfg = yaml.safe_load(ymlfile)
+
+options = webdriver.FirefoxOptions()
+options.add_argument('--headless')
 options.add_argument('window-size=1920x1080')
-browser = webdriver.Chrome(executable_path='chromedriver' , chrome_options=options)
+
+browser = webdriver.Firefox(options=options)
 browser.get('https://store.steampowered.com/')
-cookie = {'name' : 'sessionId' , 'value' : '...'}
-cookie2 = {'name' : 'steamLoginSecure', 'value' : '...'}
+
+cookie = {'name' : 'sessionId' , 'value' : cfg['steam']['sessionId']}
+cookie2 = {'name' : 'steamLoginSecure', 'value' : cfg['steam']['steamLoginSecure']}
+
 browser.add_cookie(cookie)
 browser.add_cookie(cookie2)
 browser.get('https://store.steampowered.com/')
 browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+
 try:
 	browser.find_element_by_id("discovery_queue_start_link").click()
 except:
 	browser.find_element_by_class_name('discover_queue_empty_refresh_btn').click()
+
 i = 0
-while i < 3:
-	j = 0
-	while j < 12:
+j = 0
+
+while i < cfg['loops']['first']:
+	while j < cfg['loops']['second']:
 		try:
 			browser.find_element_by_class_name('btn_next_in_queue').click()
 			j += 1
@@ -30,7 +40,7 @@ while i < 3:
 				try:
 					browser.find_element_by_class_name('refresh_queue_btn').click()
 				except:
-					print('hehe')
+					print('error')
 	i += 1
-	print(i)
+
 browser.close()
